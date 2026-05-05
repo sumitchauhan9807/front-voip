70dba4fcd326d490772ca2c880b76378c07fdad2
Sumit chauhan <sumitchauhan9807666@gmail.com>
2026-05-05 23:45:15 +0530

    wip

--- src/axios.js
+++ src/axios.js
@@ -1,6 +1,6 @@
 // api.js
 import axios from "axios";
-const production = false
+import {production} from 'src/env'
 export const baseURL = production ? 'https://backend.724voip.net/api' : "http://localhost:1337/api" 
 export const assetsURL = production ? 'https://backend.724voip.net' : "http://localhost:1337"
 export const api = axios.create({
